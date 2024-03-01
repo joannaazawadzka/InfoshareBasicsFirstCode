113cd7867f40162ce8ac47da3a9101189b2d6647
Joanna Zawadzka <joannaazawadzka@gmail.com>
2024-03-01 21:11:59 +0100

    Task two

--- InfoshareBasicsFirstCode/Program.cs
+++ InfoshareBasicsFirstCode/Program.cs
@@ -1,11 +1,11 @@
-﻿namespace InfoshareBasicsFirstCode
+﻿namespace Task1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             int number = 8;
-            
+
             Console.WriteLine("Twoja szczesliwa liczba to " + number);
         }
     }
---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 9;
+            int luckyNumber = 9;
+
+            if (number == luckyNumber)
+            {
+                Console.WriteLine("Bingo wygrales");
+
+            }
+            else
+            {
+                Console.WriteLine("Sprobuj ponownie!");
+            }
+
+            
+        }
+    }
+}
