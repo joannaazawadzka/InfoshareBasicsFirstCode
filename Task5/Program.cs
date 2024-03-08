930d544bbe65fa71106aad0e62464f91004494f9
Joanna Zawadzka <joannaazawadzka@gmail.com>
2024-03-08 09:24:42 +0100

    taskFive

---
+++ Task5/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1;
+            double b = 2;
+            char sign = '+';
+            double result = 0;
+            
+            switch (sign)
+            {
+                case '+':
+                    result = a + b; 
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':       
+                    result = a * b; 
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
